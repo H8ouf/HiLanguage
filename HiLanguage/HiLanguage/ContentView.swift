ebccd3a006e1eb56ecdef45ed0db8f3fd1902e3d
H8ouf <127872268+H8ouf@users.noreply.github.com>
2023-12-03 23:47:58 -0800

    Add files via upload

---
+++ HiLanguage/HiLanguage/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername: Float = 0
+    @State private var wrongPassword: Float  = 0
+    @State private var showingLoginScreen = false
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.purple.opacity(0.25)
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+
+                VStack {
+                    Text("HiLanguae")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.gray)
+                        .bold()
+                        .padding()
+                        
+                        
+                    Text("Login")
+                        .font(.title2)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                        
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                        }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }.navigationBarHidden(true)
+        }
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "shatha_hanouf" {
+            wrongUsername = 0
+            if password.lowercased() == "taifuni" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HiLanguage/HiLanguage/HiLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLanguageApp.swift
+//  HiLanguage
+//
+//  Created by shatha  on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HiLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
